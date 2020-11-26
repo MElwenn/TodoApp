6b1abe6d9bed3b0585b78cc03322472fac1bb352
MElwenn <63125829+MElwenn@users.noreply.github.com>
2020-11-26 21:26:34 +0100

    defined MdContent

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import App from './App.vue'
 import VueMaterial from 'vue-material'
-import { MdCard, MdField } from 'vue-material/dist/components';
+import { MdCard, MdField, MdContent } from 'vue-material/dist/components';
 import 'vue-material/dist/vue-material.min.css';
 import 'vue-material/dist/theme/default.css'
 
