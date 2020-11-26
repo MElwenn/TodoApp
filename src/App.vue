d19e1f1beb56d86095de6d913ebd3337e9cbeb7b
MElwenn <63125829+MElwenn@users.noreply.github.com>
2020-11-26 20:32:30 +0100

    final debugging, get edit todo working

--- src/App.vue
+++ src/App.vue
@@ -1,143 +1,80 @@
-<template class="body">
-  <MdContent>
-    <div class="page-container">
-      <div id="todo-items-list">
-        <h1 class="md-title">My todos</h1>
-      </div>
+<template>
+  <div class="app-container">
+    <md-toolbar style="background-color: #448aff">
+      <h1 style="color: #f5f5f5">{{ title }}</h1>
+    </md-toolbar>
 
-      <!-- Add ToDo -->
-      <md-card-content class="add-item-form">
-        <md-field>
-          <md-input
-            v-model="currentTodo"
-            @keydown.enter="addTodo()"
-            placeholder="Add a todo"
-          ></md-input>
-          <p data-v-6de59b62="" contenteditable="true" class="title"></p>
-          <MdButton class="md-primary md-raised" @click.prevent="addTodo()">
-            ADD
-          </MdButton>
-        </md-field>
-      </md-card-content>
+    <!-- Add ToDo -->
+    <md-field class="addTodo">
+      <md-input
+        v-model="currentTodo"
+        @submit.prevent="addTodo"
+        @keydown.enter.prevent="addTodo()"
+        placeholder="Add a todo"
+      >
+      </md-input>
+      <md-button
+        style="background-color: #448aff"
+        class="md-icon-button md-raised"
+        v-on:click.prevent="addTodo"
+      >
+        <md-icon style="color: #f5f5f5">add</md-icon>
+      </md-button>
+    </md-field>
 
-      <!-- Display list of added ToDos -->
-      <md-card-content class="add-item-form">
-        <ul
-          list-style="none"
-          display="inline-block"
-          class="todos"
-          list-style-type="none"
+    <!-- Display list of added ToDos -->
+    <md-list class="todos" v-if="showTodos()">
+      <md-list-item class="todo-title">To edit click and type</md-list-item>
+      <md-list-item class="list-item" v-for="todo in todos" :key="todo.id">
+        <input type="checkbox" v-model="todo.completed" />
+        <span
+          v-on:click.stop.prevent="clickToEdit(todo)"
+          v-show="editTodoId !== todo.id"
+          :class="{ completed: todo.completed }"
         >
-          <li
-            v-for="todo in filteredTodos"
-            :key="todo.id"
-            :class="{
-              todo: true,
-              editing: editedTodo === todo,
-              completed: todo.completed,
-            }"
-            @dblclick="editTodo(todo)"
-          >
-            <!-- class="md-layout md-gutter md-alignment-center" -->
-            <div class="display">{{ todo.label }}</div>
-            <md-checkbox type="checkbox" v-model="todo.completed"></md-checkbox>
-            <input type="checkbox" v-model="todo.completed" />
-            <span
-              v-on:click.stop.prevent="updateTodo(todo)"
-              v-show="editedTodoID !== todo.id"
-              :class="{ completed: todo.completed }"
-            >
-            </span>
+          {{ todo.label }}
+        </span>
 
-            <input
-              class="edit"
-              v-model="todo.label"
-              v-todo-html-focus="todo == editedTodo"
-              @blur="cancelEdit(todo)"
-              @keyup.esc="cancelEdit(todo)"
-              @keyup.enter="doneEdit(todo)"
-            />
-            <!-- CTAs to edit or remove a ToDo -->
-            <MdButton class="md-secondary" @click="editToDo(todo)">
-              EDIT
-            </MdButton>
+        <input
+          v-on:click.stop.prevent
+          v-model="todo.label"
+          v-show="editTodoId == todo.id"
+          v-on:keyup.enter="saveEdit"
+        />
 
-            <MdButton class="md-primary" @click="removeTodo(todo)"
-              ><!-- &times -->
-              X
-            </MdButton>
-          </li>
-        </ul>
-
-        <md-field>
-          <div class="items-left">{{ completed }} items left</div>
-          <div class="static">
-            <div
-              @click="visibility = 'show all'"
-              :class="{ 'items-all': true, selected: visibility === 'all' }"
-            >
-              All
-            </div>
-            <div
-              @click="visibility = 'active'"
-              :class="{
-                'items-active': true,
-                selected: visibility === 'active',
-              }"
-            >
-              Active
-            </div>
-            <div
-              @click="visibility = 'completed'"
-              :class="{
-                'items-completed': true,
-                selected: visibility === 'completed',
-              }"
-            >
-              Completed
-            </div>
-          </div>
-          <div
-            class="items-clear-completed"
-            v-if="completed !== todos.length"
-            @click="removeCompleted"
-          >
-            Clear completed
-          </div>
-        </md-field>
-      </md-card-content>
-    </div>
-  </MdContent>
+        <!-- CTAs to remove a ToDo -->
+        <md-button
+          class="md-icon-button md-raised"
+          v-on:click.prevent="removeTodo(todo)"
+        >
+          X
+        </md-button>
+      </md-list-item>
+    </md-list>
+  </div>
 </template>
 
-
-
 <script>
-import Vue from "vue";
-
-const filters = {
-  all: function (todos) {
-    return todos;
-  },
-  active: function (todos) {
-    return todos.filter((todo) => !todo.completed);
-  },
-  completed: function (todos) {
-    return todos.filter((todo) => todo.completed);
-  },
-};
-
-export default Vue.extend({
+export default {
+  name: "RegularToolbar",
   data() {
+    let todos = localStorage.getItem("todos");
+    if (todos) {
+      todos = JSON.parse(todos);
+    } else {
+      todos = [];
+    }
     return {
-      todos: [],
+      title: "My ToDos",
+      todos,
       currentTodo: "",
-      editedTodo: null,
-      initialTodo: "",
-      visibility: "all",
+      editTodoId: null,
     };
   },
   methods: {
+    showTodos() {
+      return this.todos.length > 0;
+    },
     addTodo() {
       if (this.currentTodo !== "") {
         this.todos.push({
@@ -145,97 +82,59 @@ export default Vue.extend({
           label: this.currentTodo,
           completed: false,
         });
-        //localStorage.setItem("todos", JSON.stringify(this.todos));
+        localStorage.setItem("todos", JSON.stringify(this.todos));
         this.currentTodo = "";
       }
     },
-
     removeTodo(todo) {
-      var index = this.todos.indexOf(todo);
+      const index = this.todos.indexOf(todo);
       this.todos.splice(index, 1);
-      //localStorage.setItem("todos", JSON.stringify(this.todos));
+      localStorage.setItem("todos", JSON.stringify(this.todos));
     },
-
-    removeCompleted() {
-      // hide completed todos by filter
-      this.todos = this.todos.filter((todo) => !todo.completed);
+    clickToEdit(todo) {
+      this.editTodoId = todo.id;
     },
-
-    editTodo(todo) {
-      console.log("editTodo");
-      this.editedTodo = todo;
-      this.initialTodo = todo.label;
-    },
-
-    doneEdit(todo) {
-      // ends the editing mode when keyup.enter => how to support mobile ???
-      this.editedTodo = null;
-      if (!todo.label) {
-        this.removeTodo(todo);
-      }
-    },
-
-    cancelEdit(todo) {
-      // aborts the editing mode when keyup.esc => how to support mobile ???
-      console.log("cancelEdit");
-      if (!this.editedTodo) {
-        return;
-      }
-      this.editedTodo = null;
-      todo.label = this.originalEditedTodoValue;
-    },
-
-    completed() {
-      // calculate completed todos for filtering
-      let remaining = 0;
-      this.todos.forEach((todo) => {
-        if (!todo.completed) {
-          remaining++;
-        }
-      });
-      return remaining;
-    },
-
-    filteredTodos() {
-      return filters[this.visibility](this.todos);
-    },
-
-    /*updateTodo(todo) {
-      this.editedTodoID = todo.id;
-    },*/
-
-    /*saveUpdate() {
-      this.editedTodoID = null;
-    },*/
-  },
-  directives: {
-    "todo-html-focus": function (element, binding) {
-      if (binding.value) {
-        element.focus();
-      }
+    saveEdit() {
+      this.editTodoId = null;
     },
   },
-});
+};
 </script>
 
 <style scoped>
-.body {
-  background-color: #f5f5f5;
-  text-decoration: none;
-  list-style-type: "none";
+.header {
+  background-color: #448aff;
+  color: #f5f5f5;
 }
-.display {
-  display: inline-flex;
-  text-align: left;
-  justify-content: left;
-  align-items: center;
+.addTodo {
+  margin: 0 auto;
+  margin-top: 20px;
+  margin-bottom: 20px;
+  width: 80%;
+  height: 80px;
 }
-.todo-item {
-  margin-left: 20px;
+.todos {
+  width: 60%;
+  margin: 0 auto;
+  padding-top: 0px;
+  padding-bottom: 0px;
+  border: 0.5px solid dark-grey;
 }
-.md-secondary {
-  border: 1px;
-  border-color: #448aff;
-  text-emphasis-color: #448aff;
+.todo-title {
+  background: #f5f5f5;
+}
+.list-item {
+  border-top: 0.5px solid white;
+}
+.md-button {
+  border: 1.5px solid white !important;
+}
+.md-icon-button {
+  background-color: #448aff;
+  color: #f5f5f5;
+}
+.completed {
+  text-decoration: line-through;
+  opacity: 0.5;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,11 +1,13 @@
 import Vue from 'vue'
 import App from './App.vue'
 import VueMaterial from 'vue-material'
-import { MdCard } from 'vue-material/dist/components';
+import { MdCard, MdField } from 'vue-material/dist/components';
+import 'vue-material/dist/vue-material.min.css';
+import 'vue-material/dist/theme/default.css'
 
 Vue.use(VueMaterial)
 Vue.use(MdCard);
-//Vue.use(MdCardFooter);
+Vue.use(MdField);
 
 Vue.config.productionTip = false
 
@@ -13,29 +15,3 @@ new Vue({
   render: h => h(App),
 }).$mount('#app')
 
-
-/*
-import Vue from 'vue'
-import App from './App.vue'
-import VueMaterial from 'vue-material'
-import { MdField, MdButton, MdContent, MdCard, MdCardFooter, MdList, MdCheckbox } from 'vue-material/dist/components';
-import 'vue-material/dist/vue-material.min.css';
-import 'vue-material/dist/theme/default.css';
-
-Vue.use(MdField);
-Vue.use(MdButton);
-Vue.use(MdContent);
-Vue.use(MdCard);
-Vue.use(MdCardFooter);
-Vue.use(MdList);
-Vue.use(MdCheckbox);
-//Vue.use(MdNotification);
-//Vue.use(MdApp)
-
-Vue.use(VueMaterial)
-
-Vue.config.productionTip = false
-
-new Vue({
-  render: h => h(App),
-}).$mount('#app')*/
